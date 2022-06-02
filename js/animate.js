8bdb17bbbff34e12c921680dd98d5eca4ed5460d
Elberd Chahkiev <takututu95@gmail.com>
2022-06-02 18:34:39 +0300

    add shader on header

--- js/animate.js
+++ js/animate.js
@@ -1,1 +1,74 @@
 console.log(mainSliderScreens.activeIndex)
+
+
+
+
+
+
+const slides = document.querySelectorAll('.fadeAnimateSlide')
+const fadeFromBottom = (indexSlide, index) =>{
+    if(mainSliderScreens.activeIndex === index){
+        indexSlide.style.cssText = `
+        transition: 1.5s;
+        position: relative;
+        bottom: 0px;
+        opacity: 1;`;
+        
+    } else{
+        indexSlide.style.cssText = `
+        transition: 1s;
+        position: relative;
+        bottom: -100px;
+        opacity: 0;`;
+    }
+}
+
+const grid = document.querySelectorAll('.gridAnimate')
+const gridAnimate = (index) =>{
+    
+
+    if(mainSliderScreens.activeIndex === index){
+        for(let i = 0; i < grid.length; i++){
+            setTimeout(() =>{
+                // grid[i].classList.remove('gridAnimate');
+                grid[i].style.left = '0px';
+                grid[i].style.opacity = '1';
+
+            }, 500 * (i+1))
+    
+        }
+    }
+
+
+}
+
+const coinAnimate = document.querySelector('.animateSlideCoin')
+const slideRotateAnimate = index =>{
+    
+    if(mainSliderScreens.activeIndex === index){
+        coinAnimate.style.cssText = `
+        transition: 1s;
+        transform: scale(1)`
+    }else{
+        coinAnimate.style.cssText = `
+        transition: 1s;
+        transform: scale(1.2)`
+    }
+
+
+}
+
+
+
+
+let watch = () =>{
+    for(let i = 0; i < slides.length; i++){
+        fadeFromBottom(slides[i], i)
+    }
+    gridAnimate(1)
+    slideRotateAnimate(2)
+}
+
+
+
+setInterval(()=> watch() ,100)
--- js/main.js
+++ js/main.js
@@ -1,23 +1,60 @@
-const arrow = document.getElementById('showGridInfo');
-const mainInfoToggle = document.querySelectorAll('.mainPageInfo');
 
 
+const text = document.querySelector(".header_main-title");
 
-const toggleInfo = (info, rightOrLeft) =>{
-    info.classList.toggle('lg:opacity-0');
-    info.classList.toggle('lg:invisible');
-    info.classList.toggle(rightOrLeft);
+if(screen.width > 1024){
+
+
+const hero = document.querySelector(".header_wrapp");
+const walk = 80;
+
+
+function shadow(e) {
+    const { offsetWidth: width, offsetHeight: height } = hero;
+    
+    let { offsetX: x, offsetY: y } = e;
+    
+    if (this !== e.target) {
+        x += e.target.offsetLeft;
+        y += e.target.offsetTop;
+    }
+
+    const xWalk = Math.round(x / width * walk - walk / 2);
+    const yWalk = Math.round(y / height * walk - walk / 2);
+
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 20px rgb(245, 245, 245, .5), 
+    ${xWalk}px ${yWalk}px 20px rgba(0, 0, 0, 0),
+    ${xWalk * 1.5}px ${yWalk * 1.5}px 40px rgba(0, 0, 0, 0),
+
+    ${xWalk * -1.5}px ${yWalk}px 20px rgba(48, 71, 94, .4),
+    ${xWalk * -1.2}px ${yWalk * -1.8}px 20px rgba(240, 84, 84, .2)
+    `;
 }
 
+hero.addEventListener("mousemove", shadow);
+}else{
+ text.classList.add('shadowMainspan');
+}
+    
 
 
-arrow.addEventListener('click', () => {
-    arrow.classList.toggle('-rotate-90');
-    arrow.classList.toggle('rotate-90');
-
-    toggleInfo(mainInfoToggle[0],'lg:mr-[300px]');
-    toggleInfo(mainInfoToggle[1], 'lg:mx-auto');
-
-});
 
 
+
+
+// const anchors = document.querySelectorAll('a[href*="#"]')
+
+// for (let anchor of anchors){
+//     anchor.addEventListener('click', function(e){
+//         e.preventDefault();
+        
+//         const blockID = anchor.getAttribute('href').substr(1);
+        
+//         document.getElementById(blockID).scrollIntoView({
+//             behavior: 'smooth',
+//             block: 'start'
+//         });
+        
+//     });
+// };
