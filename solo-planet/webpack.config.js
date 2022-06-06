c75d9661b3a982a4f84499f3c3ec37a0da7aa072
Elberd Chahkiev <takututu95@gmail.com>
2022-06-06 22:09:24 +0300

    fixes

---
+++ solo-planet/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+    entry: './src/js/index.js', 
+    module:{
+        rules: [
+            {test: /\.(js)%/, use: 'babel-loader'}
+        ]
+    },
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    }, 
+    mode: 'development'
+}
